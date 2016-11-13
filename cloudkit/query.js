9b0b1817411fb0a06ff2a6d6d4ce35baccdf7752
Ben Friedman <bdfd63@mail.missouri.edu>
2016-11-13 16:42:46 +0000

    removed alert test

--- cloudkit/query.js
+++ cloudkit/query.js
@@ -105,7 +105,6 @@ function fetchRecords() {
         if (response.hasErrors) {
             throw response.errors[0];
         } else {
-            window.alert(response.records.length);
             angular.forEach(response.records, function(record, key) {
                 var transaction = {name:record.recordName, description:record.fields.description.value, date:record.fields.date.value, amount:record.fields.amount.value}  
                 transactions.push(transaction);
