ca0c42df732f59b34b0cce0bcfb10361542f40be
fostera <fosteralexc@gmail.com>
2016-11-29 17:54:40 -0600

    Added route config in order to make our app have multiple "pages" while being an Angular single page application. Put everything on the home page under the "home.html" template and created a stock "expenses.html" template. Both of these may be moved to seperate template files later. Tweaked CSS, div names, class names.

--- loadCtrls.js
+++ loadCtrls.js
@@ -1,1 +1,10 @@
-var pettycash = angular.module('PettyCash', ['ngMaterial']);
+var pettycash = angular.module('PettyCash', ['ngMaterial', 'ngRoute']);
+pettycash.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "home.html"
+    })
+    .when("/expenses", {
+        templateUrl : "expenses.html"
+    })
+});
