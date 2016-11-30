57401625f633e117d2fb3cb72192c3f218075e8b
fostera <fosteralexc@gmail.com>
2016-11-30 01:13:15 -0600

    Created Expenses template for viewing transactions. Tweaked code in the expenses controller to correctly return back expenses sub-objects. Aligned material design table and modified amount to be listed on the right hand side.

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -1,27 +1,22 @@
-pettycash.controller('ExpensesController', function() {
-  
-    var expenseList = this; //scope
-    expenseList.expenses = []; //create expenses array on scope
+pettycash.controller('ExpensesController', ['$scope', '$http', function($scope, $http) {
+
     var today = Date.parse(new Date()); //today's date
-    
-    expenseList.loadExpenses = function() {
-        
-        var url = "http://ec2-54-70-114-249.us-west-2.compute.amazonaws.com:3000/authenticate";
-        
-        $http({
-            method: 'POST',
-            url: url
-        }).then(function successCallback(response) {
-        // this callback will be called asynchronously
-        // when the response is available
-            console.log("success");
-        }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-            console.log("failure");
-        });
-    };
-    
-    expenseList.loadExpenses();
-    
-});
+    var url = "http://ec2-54-70-114-249.us-west-2.compute.amazonaws.com:3000/authenticate";
+
+    $http({
+        method: 'POST',
+        url: url
+    }).then(function successCallback(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+        console.log("success");
+        console.log(response);
+        $scope.accounts = response.data.accounts;
+        $scope.expenses = response.data.transactions;
+        console.log($scope.expenses);
+    }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+        console.log("failure");
+    });
+}]);
