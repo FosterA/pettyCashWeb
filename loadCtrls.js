4b71a8a06614b0f1f39465be47e662660934f20e
fostera <fosteralexc@gmail.com>
2016-11-30 04:46:26 -0600

    In order to have our game maintain itself while on other tabs we removed the ng-routing in favor of a ng-show and ng-hide. Adjusted CSS because of course it blew up for no reason at all.

--- loadCtrls.js
+++ loadCtrls.js
@@ -1,13 +1,13 @@
-var pettycash = angular.module('PettyCash', ['ngMaterial', 'ngRoute', 'chart.js']);
-pettycash.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl : "home.html"
-    })
-    .when("/expenses", {
-        templateUrl : "expenses.html"
-    })
-    .otherwise({ redirectTo: '/'});
-}).controller('navController', function() {
-  this.currentNavItem = "Home";
-});;
+var pettycash = angular.module('PettyCash', ['ngMaterial', 'ngRoute', 'chart.js'])
+// pettycash.config(function($routeProvider) {
+//     $routeProvider
+//     .when("/", {
+//         templateUrl : "home.html"
+//     })
+//     .when("/expenses", {
+//         templateUrl : "expenses.html"
+//     })
+//     .otherwise({ redirectTo: '/'});
+.controller('navController', function() {
+  this.currentNavItem = "1";
+});
