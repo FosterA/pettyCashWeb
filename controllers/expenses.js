c3c33d8077c4aa5d87094f25621038b350df4ed7
fostera <fosteralexc@gmail.com>
2016-11-30 03:01:01 -0600

    Filter god, Ben, added a custom filter to remove the negative fomr deposits. Combined with the currency filter now deposits show as green and withdrawls show as red.

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -9,14 +9,20 @@ pettycash.controller('ExpensesController', ['$scope', '$http', function($scope, 
     }).then(function successCallback(response) {
     // this callback will be called asynchronously
     // when the response is available
-        console.log("success");
-        console.log(response);
         $scope.accounts = response.data.accounts;
         $scope.expenses = response.data.transactions;
-        console.log($scope.expenses);
+
     }, function errorCallback(response) {
     // called asynchronously if an error occurs
     // or server returns response with an error status.
         console.log("failure");
     });
-}]);
+}]).filter('deposit', function() { //filter for displaying priority as text
+    return function(expenseAmt) {
+        if (expenseAmt < 0) {
+          return expenseAmt.toString().slice(1);
+        } else {
+          return expenseAmt;
+        }
+    }
+});
